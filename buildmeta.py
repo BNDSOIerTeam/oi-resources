91d8c34c6ed0ce4add026fb9dd992ea51f0f0083
Colinxu2020 <colinxu2020@gmail.com>
2023-10-05 14:16:44 +0800

    upd workflow

--- buildmeta.py
+++ buildmeta.py
@@ -1,1 +1,10 @@
-print('test ')
+# Scan to build pages.json
+import pathlib
+
+from markdown import markdown
+
+
+for dir in pathlib.Path('.').glob('**'):
+    result = []
+    for file in dir.glob('*.md'):
+        print(markdown(file.read_text(), extensions = ['meta']))
